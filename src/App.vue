7bfd608889926790b3582c531fcd5070a81daf2e
fufaich <fufaich@gmail.com>
2024-01-11 16:24:58 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+   import MyHello from '@/components/MyHello.vue'
+</script>
+
+<template>
+  <div
+    class=""
+  >
+
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/MyHello.vue
@@ -0,0 +1,22 @@
+<script setup>
+  defineProps({
+    title: String,
+    price: Number,
+  })
+</script>
+
+
+<template>
+  <div class="product">
+    <h2>Товар: {{ title }}</h2>
+    <h3>Цена: {{ price }}</h3>
+  </div>
+</template>
+
+<style scoped>
+.product{
+  border: 1px solid blue;
+  padding: 10px;
+  margin-bottom: 10px;
+}
+</style>
